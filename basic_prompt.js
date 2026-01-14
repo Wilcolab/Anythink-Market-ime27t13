85183cf7feaa011e267d54fc560af041ec5cc8e2
WilcoApp <hello@wilco.gg>
2026-01-14 15:20:26 +0000

    Initial commit

--- basic_prompt.js
+++
@@ -1,9 +0,0 @@
-function toCamelCase(text) {
-    return text
-        .split(/[\s_]+/)
-        .map((word, index) => {
-            const lower = word.toLowerCase();
-            return index === 0 ? lower : lower.charAt(0).toUpperCase() + lower.slice(1);
-        })
-        .join('');
-}
